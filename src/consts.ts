cfdcbda8ef86aa36129ef9cb67a404681a25f1bb
Tomohiro Yoshida <rollwithit0117@gmail.com>
2023-06-24 21:50:59 +0900

    feat: ページ送り

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Blog";
-export const SITE_DESCRIPTION = "Someone's blog";
+export const SITE_TITLE = "誰かのやつ";
+export const SITE_DESCRIPTION = "";
 export const noImageUrl = "/images/eyecatch/noimage.png";
 export const profileIcon100Url = "/images/icon/yoshi_skyblue_100.png";
+export const profileIcon400Url = "/images/icon/yoshi_skyblue_400.png";
+export const postCountPerPage = 9;
---
+++ src/types/pageTypes.ts
@@ -0,0 +1,5 @@
+export type PageNumbers = {
+    prev: number
+    current: number
+    next: number
+}
